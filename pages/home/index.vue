94ec1b178d04422d306f6f4da607597a19f7a019
Mutlaq Aldhbuiub <matulkn@gmail.com>
2021-06-19 05:08:19 +0000

    Creating home page & route , creating card component

---
+++ pages/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="pt-5">
+  <div class="row">
+      <div v-for="i in 25" :key="i" class="col-lg-4 col-md-4 pb-4 px-4">
+        <Card :card="card" background_image="true" />
+      </div>
+      
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    layout:'auth',
+    data:() => {
+        return {
+            card:{
+                title:"hello wosdrld!!!",
+                background: 'https://source.unsplash.com/random/200x200?sig='
+            },
+            background_image: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
