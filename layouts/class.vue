ff22110f0d48ec4eab21a1245ac9165bd816ddf9
Mutlaq Aldhbuiub <matulkn@gmail.com>
2021-06-28 07:46:16 +0000

    Adding classes page, layout

---
+++ layouts/class.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <Navbar />
+    <div class="container">
+        <section>
+            <div class="row">
+                <div class="col-3">
+                    <Sidebar />
+                </div>
+                <div class="col-9">
+                    <Nuxt />
+                </div>
+            </div>
+        </section>
+    </div>
+     <Footer />
+</div>
+</template>
+
+<script>
+export default {
+  head:{
+    title:'Class',
+    script: [
+      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' },
+      { type: 'text/javascript', src: '/js/bootstrap-navbar.js'},
+      { type: 'text/javascript', src:'/js/theme.js'}
+    ]
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/class/_id/homeworks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-card>
+      <div class="card-body">
+        Homeworks
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  layout:'class',
+}
+</script>
+
+<style>
+
+</style>
